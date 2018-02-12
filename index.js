6a553f7d14021b84209d476931fb89d045d20eb1
Daren Capacio <megamanv5@hotmail.com>
2018-02-12 04:14:46 -0800

    testing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');  
 var app = express();  
-var server = require('http').Server();  
+var server = require('http').Server(app);  
 var io = require('socket.io')(server);
 
 app.set('port', process.env.PORT || 5000);
