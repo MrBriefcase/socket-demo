15ba688cbe0a5b14e996fde970f1498aaf501624
Daren Capacio <megamanv5@hotmail.com>
2018-02-12 03:10:01 -0800

    testing

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ var io = require('socket.io')(server);
 
 app.set('port', process.env.PORT || 5000);
 
+// ALLOW ACCESS *************
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://socketpractice.herokuapp.com");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
     res.send('Port is: ' + hold);
