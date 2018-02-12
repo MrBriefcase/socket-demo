e7c2b7c039504ff20aca89bc0a7bdeabd526e2c7
Daren Capacio <megamanv5@hotmail.com>
2018-02-11 22:27:13 -0800

    testing

---
+++ index.js
@@ -0,0 +1,24 @@
+const server = require("http").Server();
+const port = process.env.PORT || 5000;
+
+var io = require("socket.io")(server);
+
+//this sends a message called joined
+io.on("connection", function(socket){
+    io.emit("joined");
+	socket.on("typing", function(){
+		socket.broadcast.emit("isTyping");
+	});
+});
+
+server.listen(port, function(err){
+    if (err){
+        console.log("there is a problem");
+        return false;
+    }
+    else{
+        console.log("all good head over to the server");
+    }
+});
+
+
