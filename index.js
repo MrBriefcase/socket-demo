0f9cf1fb4205f983ffad694bd4df89365b274165
Daren Capacio <megamanv5@hotmail.com>
2018-02-12 02:49:10 -0800

    testing

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const server = require("http").Server();
-const port = process.env.PORT || 5000;
+var express = require('express');  
+var app = express();  
+var server = require('http').createServer(app);  
+var io = require('socket.io')(server);
 
-alert('port is ' + port);
-
-var io = require("socket.io")(server);
-
-//this sends a message called joined
-io.on("connection", function(socket){
-    io.emit("joined");
-	socket.on("typing", function(){
-		socket.broadcast.emit("isTyping");
-	});
+app.use(express.static(__dirname + '/node_modules'));  
+app.get('/', function(req, res, next) {  
+    res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(port, function(err){
-    if (err){
-        console.log("there is a problem");
-        return false;
-    }
-    else{
-        console.log("all good head over to the server");
-    }
-});
-
-
+server.listen(4200);  
