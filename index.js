05587619c4d29e6c11c89cd69a7de6f83a8dc913
Daren Capacio <megamanv5@hotmail.com>
2018-02-12 02:54:18 -0800

    testing

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ var app = express();
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 
+app.set('port', process.env.PORT || 5000);
+
 app.use(express.static(__dirname + '/node_modules'));  
 app.get('/', function(req, res, next) {  
     res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(process.env.PORT);  
+server.listen(app.get('port'), function(){
+    //success
+});  
